25170ec02d8020aa2ce943cd64e7f87a6756cd8e
sangrammule1 <57299504+sangrammule1@users.noreply.github.com>
2025-10-08 18:01:19 +0530

    Fix NullPointerException in src/App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) {
         String message = null;
-        System.out.println(message.length()); // NullPointerException
+        System.out.println(message == null ? 0 : message.length());
     }
 }
