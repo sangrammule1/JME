1dcfd26beef83fd337ab2a6e6696f1c5187414c7
Sangram Mule <sangrammule@Sangrams-MacBook-Air.local>
2025-10-08 20:24:42 +0530

    commit 1

--- src/App.java
+++ src/App.java
@@ -1,6 +1,8 @@
 public class App {
     public static void main(String[] args) {
         String message = null;
-        System.out.println(message == null ? 0 : message.length());
+        Integer length = message.length();
+
+        System.out.println(length);
     }
 }
