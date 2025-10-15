5b58f6af3af72200755499c70a9461232b94c97c
Sangram Mule <sangrammule@Sangrams-MacBook-Air.local>
2025-10-15 10:50:37 +0530

    commit 6

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public static void main(String[] args) {
         Integer a = 2;
         Integer b= 0;
-        Integer c = (b == 0) ? 0 : (a / b);
+        Integer c = a/b;
         System.out.println(c);
     }
 }
