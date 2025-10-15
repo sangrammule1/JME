e151fcab6f8dbef05ab87e0b7b734935f90748ab
sangrammule1 <57299504+sangrammule1@users.noreply.github.com>
2025-10-15 10:50:59 +0530

    Fix ArithmeticException in src/Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     public static void main(String[] args) {
         Integer a = 2;
         Integer b= 0;
-        Integer c = a/b;
+Integer c = (b == 0) ? 0 : (int) (a / b);
         System.out.println(c);
     }
 }
